413592b77402cfe48bad674fc05741bd0dade1a4
Richard Burd <richardburd@yahoo.com>
2022-09-15 23:07:18 +0000

    head & title on app to display on every page

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,9 @@ const Layout = ({ children }) => {
       {/* https://tailwindcss.com/docs/installation/play-cdn
       this is a tempoary script for copy & pasting to Michael's code...
       ...in the final build, this will be deleted and we will use the command:
-      $npm run build-css 
+      $npm run build-css
+      ...but for now, we will copy & paste the <script> tag below into each <head> tag
+      that we send Michael for him to integrate into his build 
         <script src="https://cdn.tailwindcss.com"></script>*/}
       <Navbar />
       <div className="page-content">{children}</div>
---
+++ components/SpecializedSiteButton.js
@@ -0,0 +1,5 @@
+const SpecializedSiteButton = () => {
+    return <div>this is a specialized site button</div>
+}
+ 
+export default SpecializedSiteButton;
