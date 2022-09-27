bd0651743412552e4d8a839f7f39522b63d5e2d9
Richard Burd <richardburd@yahoo.com>
2022-09-27 18:54:32 +0000

    experimental iterations on multi_solutions.json

--- pages/energy/index.js
+++ pages/energy/index.js
@@ -1,27 +1,50 @@
 import Head from "next/head";
-const data = require("../../data/multi_solutions.json")
-
+const data = require("../../data/multi_solutions.json");
 
 const Energy = () => {
-    // Talk to Chris bloom before you try any of this below
-    console.log(data[0].topics)
-    // iterate over the data's array & find the object whith the key of name with the value of "energy"
-    // display the intro on the page (console.log(data[0].intro))
+  // This iterates over Michael's "multi_solutions.json" and finds the "energy" site
+  // from here
+  data.map((item, index) => {
+    if (data[index].site === "energy") {
+      console.log("i just found the energy site");
+      console.log(`Here i am printing out the name of the site: ${data[index].site}`);
+    }
+  })
+  // Talk to Chris bloom before you try any of this below
+  console.log(data[0].topics);
 
-    // once in the energy object, iterate over the topics array & 
-    // display the topics on the page {console.log(data[0].topics)}
-    // create buttons for those topics
-    return (
-        <>
-          <Head>
-            <title>UCS | Energy Site</title>
-            <meta name="keywords" content="energy, energy research, carbon pricing, solar, wind, biomass, nuclear, coal" />
-          </Head>
-          <div className="m-12 p-12 text-8xl">
-            this is the &quot;Energy&quot; specialized site
+  // iterate over the data's array & find the object whith the key of name with the value of "energy"
+  // display the intro on the page (console.log(data[0].intro))
+
+  // once in the energy object, iterate over the topics array &
+  // display the topics on the page {console.log(data[0].topics)}
+  // create buttons for those topics
+  const focusAreas = data[0].topics;
+
+  return (
+    <>
+      <Head>
+        <title>UCS | Energy Site</title>
+        <meta
+          name="keywords"
+          content="energy, energy research, carbon pricing, solar, wind, biomass, nuclear, coal"
+        />
+      </Head>
+      <div className="m-12 p-12 text-8xl">
+        this is the &quot;Energy&quot; specialized site
+      </div>
+      <div>
+        <h1>Focus Areas:</h1>
+        {focusAreas.map((focusAreas) => (
+          <div key={focusAreas.url}>
+            <a>
+              <h3>{focusAreas.name}</h3>
+            </a>
           </div>
-        </>
-      );
-}
- 
+        ))}
+      </div>
+    </>
+  );
+};
+
 export default Energy;
