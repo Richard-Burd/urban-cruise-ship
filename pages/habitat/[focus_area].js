288959836ebc39532b948dba42c6b7a4c09f95f8
Richard Burd <richardburd@yahoo.com>
2022-09-28 17:04:52 +0000

    created 3rd experimental schema using HABITAT site

---
+++ pages/habitat/[focus_area].js
@@ -0,0 +1,25 @@
+import { Router, useRouter } from 'next/router'
+
+// // https://youtu.be/mAHqpdVzJmA?t=208
+// export const getStaticPaths = async () => {
+//   // do we really need this?
+// }
+
+// const paths = data.map(focus_area => {
+//   return {
+    
+//   }
+// })
+
+// below I can 
+const FocusArea = () => {
+  const router = useRouter()
+  console.log(router.asPath)
+  return (
+    <div>
+      <h1>This component is for the focus area called: {router.asPath} </h1>
+    </div>
+  );
+};
+
+export default FocusArea;
--- pages/habitat/index.js
+++ pages/habitat/index.js
@@ -1,17 +1,55 @@
 import Head from "next/head";
+import Link from "next/link";
+const data = require("./focusAreas.json");
+
+console.log(data);
+
+data.map((item) => {
+  console.log(item.title);
+});
 
 const Habitat = () => {
-    return (
-        <>
-          <Head>
-            <title>UCS | Habitat Site</title>
-            <meta name="keywords" content="habitat, diet, health, population, social well-being, environment, Consumerism" />
-          </Head>
-          <div className="m-12 p-12 text-8xl">
-            this is the &quot;Habitat&quot; specialized site
-          </div>
-        </>
-      );
-}
- 
+  return (
+    <>
+      <Head>
+        <title>UCS | Habitat Site</title>
+        <meta
+          name="keywords"
+          content="habitat, diet, health, population, social well-being, environment, Consumerism"
+        />
+      </Head>
+      <nav>
+        {data.map((item) => (
+          <Link href={"/habitat/" + item.url} key={item.url}>
+            <a>
+              <span className="m-4 p-4">{item.title}</span>
+            </a>
+          </Link>
+        ))}
+      </nav>
+      <div className="mt-12">
+        the Navbar above will appear in the HABITAT site and child focus area
+        sites; the data to generate this navbar comes from the
+        ./pages/habitat/focusAreas.json file
+      </div>
+      <div className="m-12 p-12 text-8xl">
+        this is the &quot;Habitat&quot; specialized site
+      </div>
+      <div className="m-12 p-12 text-4xl">
+        The Focus Areas below are being dynamically rendered per from:
+        ./pages/habitat/focusAreas.json
+      </div>
+      <div>
+        {data.map((item) => (
+          <Link href={"/habitat/" + item.url} key={item.url}>
+            <a>
+              <h3>{item.title}</h3>
+            </a>
+          </Link>
+        ))}
+      </div>
+    </>
+  );
+};
+
 export default Habitat;
