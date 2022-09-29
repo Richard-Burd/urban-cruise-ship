6e749e8e5f4a1806fd5d54f0cf8dcc09f36458f0
Richard Burd <richardburd@yahoo.com>
2022-09-29 01:06:51 +0000

    pulled select data from JSON hierarchy in proof of concept

--- pages/cities/cities_socioeconomics/index.js
+++ pages/cities/cities_socioeconomics/index.js
@@ -4,12 +4,33 @@
 // If I had information outside the hierarchy that I
 // wanted to put on this focus area landing page,
 // I could add that in here.
+
+// I want to go in and find this:
+// "focus_area_url":"cities_socioeconomics",
+// ...then render only the children therein
 const data = require("../hierarchy.json");
+
+function findChildren(searchValue) {
+  return data.find(({ focus_area_url }) => {
+    return focus_area_url === searchValue;
+  }).articles
+}
+console.log(findChildren("cities_socioeconomics"))
 
 const citiesSocioeconomics = () => {
   return (
     <>
       <div>CITIES Specialized Site | Socioeconomics Focus Area</div>
+      <div>Chris Bloom will help me render the children I am looking for</div>
+      <div>
+        {findChildren("cities_socioeconomics").map((article) => {
+          return (
+            <div key={article.article_url}>
+              <a href={article.article_url}>{article.article_title}</a>
+            </div>
+          )
+        })}
+      </div>
     </>
   );
 };
