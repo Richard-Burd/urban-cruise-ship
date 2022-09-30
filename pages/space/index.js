6fb86200f520274c18adb54460042b594c9cee97
Richard Burd <richardburd@yahoo.com>
2022-09-30 03:19:31 +0000

    space site done

--- pages/space/index.js
+++ pages/space/index.js
@@ -1,17 +1,26 @@
 import Head from "next/head";
+const hierarchy = require("./hierarchy.json");
+import SpecializedSite from "../../components/SpecializedSite";
+
+const siteConfig = require("./site_config.json");
+const site = siteConfig.specialized_site_name;
 
 const Space = () => {
-    return (
-        <>
-          <Head>
-            <title>UCS | Space Site</title>
-            <meta name="keywords" content="space, orbital debris, planetary protection, life, matter, scale, time" />
-          </Head>
-          <div className="m-12 p-12 text-8xl">
-            this is the &quot;Space&quot; specialized site
-          </div>
-        </>
-      );
-}
- 
+  return (
+    <>
+      <Head>
+        <title>UCS | Space Site</title>
+        <meta
+          name="keywords"
+          content="space, orbital debris, planetary protection, life, matter, scale, time"
+        />
+      </Head>
+      <SpecializedSite
+        site={site}
+        focusAreas={hierarchy}
+      />
+    </>
+  );
+};
+
 export default Space;
