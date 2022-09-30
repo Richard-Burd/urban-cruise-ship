7ef76a5080d3ab8214c311e7165d0458dec39e1d
Richard Burd <richardburd@yahoo.com>
2022-09-30 17:18:03 +0000

    History site iterations complete

---
+++ pages/history/endeavors/index.js
@@ -0,0 +1,35 @@
+import FocusArea from "../../../components/FocusArea";
+
+const siteConfig = require("../site_config.json");
+const site = siteConfig.specialized_site_name;
+
+const hierarchy = require("../hierarchy.json");
+function findFocusAreaName(searchValue) {
+  return hierarchy.find(({ focus_area_url }) => {
+    return focus_area_url === searchValue;
+  }).focus_area_name;
+}
+
+// The Focus Area URL is inputted here,
+// it should be the same as the folder name,
+// You will have to change this each time you 
+// create or move a focus area.
+const focusAreaUrl = "endeavors";
+
+const focusAreaName = findFocusAreaName(focusAreaUrl);
+
+const HistoryEndeavors = () => {
+  return (
+    <>
+      <FocusArea
+        site={site}
+        focusAreaName={focusAreaName}
+        focusAreaUrl={focusAreaUrl}
+        hierarchy={hierarchy}
+      />
+      <div className={`${site}-background-gradient`}>a link to Michael&apos;s endeavors page will go here</div>
+    </>
+  );
+};
+
+export default HistoryEndeavors;
--- pages/history/index.js
+++ pages/history/index.js
@@ -1,17 +1,26 @@
 import Head from "next/head";
+const hierarchy = require("./hierarchy.json");
+import SpecializedSite from "../../components/SpecializedSite";
+
+const siteConfig = require("./site_config.json");
+const site = siteConfig.specialized_site_name;
 
 const History = () => {
-    return (
-        <>
-          <Head>
-            <title>UCS | History Site</title>
-            <meta name="keywords" content="history, timelines, endeavors, cost, benefit" />
-          </Head>
-          <div className="m-12 p-12 text-8xl">
-            this is the &quot;History&quot; specialized site
-          </div>
-        </>
-      );
-}
- 
+  return (
+    <>
+      <Head>
+        <title>UCS | History Site</title>
+        <meta
+          name="keywords"
+          content="history, timelines, endeavors, cost, benefit"
+        />
+      </Head>
+      <SpecializedSite
+        site={site}
+        focusAreas={hierarchy}
+      />
+    </>
+  );
+};
+
 export default History;
