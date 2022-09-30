295e93e45785c890420435a50baa6195b065cb46
Richard Burd <richardburd@yahoo.com>
2022-09-30 02:37:37 +0000

    cities site finished

--- pages/waste/index.js
+++ pages/waste/index.js
@@ -1,17 +1,23 @@
 import Head from "next/head";
+const hierarchy = require("./hierarchy.json");
+import SpecializedSite from "../../components/SpecializedSite";
 
 const Waste = () => {
-    return (
-        <>
-          <Head>
-            <title>UCS | Waste Site</title>
-            <meta name="keywords" content="waste, recycling, composition, landfill, wet, dry, funding, hazardous waste, material reduction" />
-          </Head>
-          <div className="m-12 p-12 text-8xl">
-            this is the &quot;Waste&quot; specialized site
-          </div>
-        </>
-      );
-}
- 
+  return (
+    <>
+      <Head>
+        <title>UCS | Energy Site</title>
+        <meta
+          name="keywords"
+          content="waste, recycling, composition, landfill, wet, dry, funding, hazardous waste, material reduction"
+        />
+      </Head>
+      <SpecializedSite
+        site={"waste"}
+        focusAreas={hierarchy}
+      />
+    </>
+  );
+};
+
 export default Waste;
