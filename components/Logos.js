82203013a12b2d920f7723983bbf9691798c83e9
Richard Burd <richardburd@yahoo.com>
2022-10-09 21:09:27 +0000

    refined animations for site buttons

--- components/Logos.js
+++ components/Logos.js
@@ -1,18 +1,65 @@
 import Image from "next/image";
+import { motion } from "framer-motion";
+import { useRouter } from "next/router";
 
 const Logos = (props) => {
+  // We'll use Next.js's router to get the current page's path
+  const router = useRouter();
+
+  // First, we'll see if the current page is a specialized site or a subdirectory
+  // of a specialized site.
+  const isASubDirectory = router.pathname.includes(`/${props.site}/`);
+
+  // If the current page is a subdirectory of a specialized site, the
+  // specialized site's logo will be an active button with a link and motion
+  // ...these will be spelled out in the TailwindCSS code
+
+  // If the current page is site homepage, then the specialized site's logo
+  // will just be an image with no user interactions.
+
+  // below we will use the ternary operator to determine which logo to render
+
   return (
     <>
-      <nav className="flex justify-between mx-8">
-        <div className="">
-          <Image
-            src={`/images/specialized-site-buttons/${props.site}.svg`}
-            alt={props.site}
-            height={270}
-            width={410}
-            priority
-          />
-        </div>
+      <nav className="flex justify-between mx-8 overflow-visible">
+        {isASubDirectory ? (
+          <motion.div
+            initial={{ x: -400, y: 10, scale: 1.4, opacity: 0, rotate: 3 }}
+            animate={{
+              x: 0,
+              y: 0,
+              scale: 1,
+              opacity: 1,
+              rotate: 0,
+            }}
+            transition={{ duration: 0.3, type: "spring" }}
+          >
+            <div className="active:scale-100 transition hover:-translate-y-1 hover:scale-105 duration-200">
+              <a href={`/${props.site}`}>
+                <Image
+                  src={`/images/specialized-site-buttons/${props.site}.svg`}
+                  alt={props.site}
+                  height={270}
+                  width={410}
+                  priority
+                />
+              </a>
+            </div>
+          </motion.div>
+        ) : (
+          <div>
+            <a>
+              <Image
+                src={`/images/specialized-site-buttons/${props.site}.svg`}
+                alt={props.site}
+                height={270}
+                width={410}
+                priority
+              />
+            </a>
+          </div>
+        )}
+
         <div className="mr-2">
           <Image
             src={`/images/ucs-logo.svg`}
--- components/SpecializedSiteNavbarButton.js
+++ components/SpecializedSiteNavbarButton.js
@@ -1,8 +1,7 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { useState } from "react";
-import { motion, useScroll } from "framer-motion";
-
+// import { useRouter } from "next/router";              // currently not in use
+// import { useState } from "react";                     // currently not in use
+import { motion /* useScroll */ } from "framer-motion"; // useScroll() currently not in use
 
 const SpecializedSiteNavbarButton = (props) => {
   // We are using the router to see what the current page is
@@ -18,7 +17,7 @@ const SpecializedSiteNavbarButton = (props) => {
   // We will track the current page & site passed down in the props
   // by tracking the current state
   // const [isCorrectSite, setCorrectSite] = useState(false);
- 
+
   // https://www.framer.com/docs/scroll-animations/
   // const { scrollYProgress } = useScroll();
 
@@ -26,15 +25,29 @@ const SpecializedSiteNavbarButton = (props) => {
     <>
       <motion.div
         initial={{ x: "-20vw", scale: 2, opacity: 0, rotate: -5 }}
-        animate={{ x: 0, scale: 1, opacity: 1, rotate: 0 }}
-        transition={{
-          delay: 0.1,
-          duration: 0.6,
-          type: "spring",
-          stiffness: 100,
+        animate={{
+          x: 0,
+          y: 0,
+          scale: 1,
+          opacity: 1,
+          rotate: 0,
+          transition: {
+            delay: 0.1,
+            duration: 0.6,
+            type: "spring",
+            stiffness: 100,
+          },
         }}
-        whileHover={{ /* TailwindCSS is better than FramerMotion for this */ }}
-        whileTap={{ /* TailwindCSS is better than FramerMotion for this */ }}
+        whileHover={
+          {
+            /* TailwindCSS is better than FramerMotion for this */
+          }
+        }
+        whileTap={
+          {
+            /* TailwindCSS is better than FramerMotion for this */
+          }
+        }
       >
         <button
           className={`elliptical-geometry ${props.site}-site-button-color ${props.site}-site-button-font-color mt-4 mb-3 px-3 py-0.5 specialized-site-navbar-shadow tracking-wide transition hover:-translate-y-1 hover:scale-110 duration-100 active:scale-100 uppercase`}
