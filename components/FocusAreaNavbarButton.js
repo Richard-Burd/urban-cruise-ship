36dcff09d7c2a4f0e69bc087b5ab2b460d51dba7
Richard Burd <richardburd@yahoo.com>
2022-10-10 17:55:05 +0000

    spacing in focus-area-navbar fixed

--- components/FocusAreaNavbarButton.js
+++ components/FocusAreaNavbarButton.js
@@ -19,7 +19,7 @@ const FocusAreaNavbarButton = (props) => {
         transition={{ duration: 0.5, type: "spring" }}
       >
         <button
-          className={`focus-area-navbar-border-width border-current ${props.site}-site-button-color ${props.site}-site-focus-area-button-border-n-font-color focus-area-navbar-elliptical-geometry focus-area-navbar-shadow mb-3.5 mt-2.5 px-3.5 mx-2.5 standard-font-1 hover:-translate-y-1 hover:scale-110 duration-100 active:scale-100 tracking-wide`}
+          className={`focus-area-navbar-border-width border-current ${props.site}-site-button-color ${props.site}-site-focus-area-button-border-n-font-color focus-area-navbar-elliptical-geometry focus-area-navbar-shadow px-3.5 mx-2.5 my-2 standard-font-1 hover:-translate-y-1 hover:scale-110 duration-100 active:scale-100 tracking-wide`}
         >
           {props.focusAreaName}
         </button>
