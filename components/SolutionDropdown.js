e67f25847f86864362db1f54aa0296b6dae2cdb6
Richard Burd <richardburd@yahoo.com>
2022-10-14 23:25:11 +0000

    solutions dropdown under construction

---
+++ components/SolutionDropdown.js
@@ -0,0 +1,11 @@
+const SolutionDropdown = ({children}) => {
+  return (
+    <>
+      <div className="bg-slate-300 m-4 p-4">
+        <div>{children}</div>
+      </div>
+    </>
+  );
+};
+
+export default SolutionDropdown;
