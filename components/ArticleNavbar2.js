42d02f768bc0f78c39c5ff356ea7f668b67eed9d
Richard Burd <richardburd@yahoo.com>
2022-10-21 00:06:38 +0000

    images added to OCEANS site

--- components/ArticleNavbar2.js
+++ components/ArticleNavbar2.js
@@ -15,6 +15,7 @@ const ArticleNavbar2 = (props) => {
       <div className="grid grid-cols-2 lg:grid-cols-5 gap-x-4 gap-y-0.5 mr-3 ml-2 lg:mr-0 lg:ml-0 my-2">
         {articleSet.map((article) => (
           <Link
+            key={article.article_url}
             href={`/${props.site}/${props.focusAreaUrl}/${article.article_url}`}
           >
             <a key={article.article_url} target="_blank" rel="noreferrer">
