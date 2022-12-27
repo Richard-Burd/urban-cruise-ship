a89a8449fc698df97a860c8f8950da1f274d56b5
Richard Burd <richardburd@yahoo.com>
2022-12-27 02:53:02 +0000

    changed 'Footnotes' to 'References'

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 // https://github.com/vercel/next.js/discussions/36310#discussioncomment-2602491
 import remarkGfm from "remark-gfm";
 import createMDX from "@next/mdx";
+import remarkRehype from 'remark-rehype'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -15,13 +16,8 @@ const nextConfig = {
 const withMDX = createMDX({
   extension: /\.mdx?$/,
   options: {
-    // we want to modify the remarkGfm plugin so that it
-    // renders 'References' instead of 'Footnotes'.
-    // We tried running the following line of code here:
-    // remarkPlugins: [() => remarkGfm({label: 'references'})], 
-    // but it didn't work.
     remarkPlugins: [remarkGfm],
-    rehypePlugins: [],
+    rehypePlugins: [[remarkRehype, {footnoteLabel: "References"}]],
   },
 });
 
