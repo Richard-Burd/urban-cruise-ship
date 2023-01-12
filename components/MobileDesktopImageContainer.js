e34e3fcc62f76593b6affce92d92e539ac79d6de
Richard Burd <richardburd@yahoo.com>
2023-01-12 18:37:11 +0000

    Container 4 mobile & desktop images

---
+++ components/MobileDesktopImageContainer.js
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import { useState, useEffect } from "react";
+
+// This is where the images are stored on the ucs-images repository
+export const path = `${process.env.NEXT_PUBLIC_ARTICLE_IMAGES_URI_PATH}`;
+
+const MobileDesktopImageContainer = ({ mobileSrc, desktopSrc, mobileHeight, mobileWidth, desktopHeight, desktopWidth, }) => {
+  const [width, setWidth] = useState(window.innerWidth);
+  const [imageSrc, setImageSrc] = React.useState(src);
+
+  useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // If there is a mobile image, us it in mobile view
+  // If there is a desktop image, use it in desktop view
+  // If there is no desktop image, use the mobile image in desktop view
+  return (
+    <>
+      <div className="flex justify-center px-4">
+        {width < 768 ? (
+          <Image 
+            src={mobileSrc} 
+            alt={`The image: "${mobileSrc}" cannot be found!`}
+            height={mobileHeight}
+            width={mobileWidth}
+          />
+        ) : (
+          <Image 
+            src={desktopSrc} 
+            alt={`The image: "${desktopSrc}" cannot be found!`}
+            height={desktopHeight}
+            width={desktopWidth}
+            onError={() => {
+              imageSrc = mobileSrc;
+              setImageSrc(mobileSrc)
+            }} 
+          />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default MobileDesktopImageContainer;
