93d023b432ec529f025cd9d1c44da1dfd21ae34d
jyeart <96083227+jyeart@users.noreply.github.com>
2023-02-16 13:11:49 -0600

    updated metrics for ship speed limits.
    the numbers were unexpectedly different. some lower and some way higher.

--- solutionMetrics.js
+++ solutionMetrics.js
@@ -2,16 +2,16 @@
 // Solutions Metrics on the Accomplishments Page:
 
 // Benefit of all our solutions (trillions)
-export const totalBenefitInTrillions = 2.546;
+export const totalBenefitInTrillions = 2.518;
 
 // Cost of all our solutions (trillions)
-export const totalCostInTrillions = 1.453;
+export const totalCostInTrillions = 1.468;
 
 // MTCO₂e Avoided (Millions)
-export const totalMTCO2eInMillions = 1.086;
+export const totalMTCO2eInMillions = 996;
 
 // Habitat Saved (Millions Km²)
 export const totalHabitatPreservedMillionsKm2 = 78.34;
 
 // Date of last change to this file in "parentheses"
-export const dateOfLastChange = "February 8, 2023"
+export const dateOfLastChange = "February 16, 2023"
