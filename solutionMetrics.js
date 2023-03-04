f62570c5a2867dd8b205fc735b22b9d64aa5fb69
jyeart <96083227+jyeart@users.noreply.github.com>
2023-03-04 08:08:17 -0600

    Update solutionMetrics.js

--- solutionMetrics.js
+++ solutionMetrics.js
@@ -2,16 +2,16 @@
 // Solutions Metrics on the Accomplishments Page:
 
 // Benefit of all our solutions (trillions)
-export const totalBenefitInTrillions = 2.518;
+export const totalBenefitInTrillions = 4.491;
 
 // Cost of all our solutions (trillions)
-export const totalCostInTrillions = 1.468;
+export const totalCostInTrillions = 3.179;
 
 // MTCO₂e Avoided (Millions)
-export const totalMTCO2eInMillions = 996;
+export const totalMTCO2eInMillions = 2704;
 
 // Habitat Saved (Millions Km²)
 export const totalHabitatPreservedMillionsKm2 = 78.34;
 
 // Date of last change to this file in "parentheses"
-export const dateOfLastChange = "February 16, 2023"
+export const dateOfLastChange = "March 3, 2023"
