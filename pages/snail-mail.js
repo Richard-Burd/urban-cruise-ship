c9ac99ead9637040c5549ec1302ff9c623ebf0a5
Richard Burd <richardburd@yahoo.com>
2023-03-13 20:22:50 +0000

    John's snail-mail revisions

--- pages/snail-mail.js
+++ pages/snail-mail.js
@@ -19,9 +19,18 @@ const SnailMail = () => {
             Letters to John can be sent to the following address:
           </h2>
           <div className="address-set text-2xl ml-4 sm:ml-20 tracking-wide">
-            <div>1407 Roberts Ave.</div>
-            <div>Nashville, TN</div>
-            <div>37206-2524 </div>
+            <div>To minimize resource consumption, including time, please send only respectful , personal communication to:</div>
+            <div className="ml-6 mt-4 p-4 bg-slate-200 inline-block max-w-max">
+              <div><b>Mailing address:</b></div>
+              <div>John van der Harst</div>
+              <div>1407 Roberts Ave.</div>
+              <div>Nashville, TN 37206</div>
+              <div>U.S.A.</div>
+            </div>
+            <div className="mt-20">Telephone, preferably as close as possible to 9:00-11:00 pm Central U.S. time (CST):</div>
+            <div className="ml-6 mt-4 p-4 bg-slate-200 inline-block max-w-max">
+              <div>615-227-3499</div>
+            </div>
           </div>
         </div>
       </div>
