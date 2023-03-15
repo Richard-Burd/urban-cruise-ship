367b0b469cfeaee85b888f5317dab8f5d3a48218
Richard Burd <richardburd@yahoo.com>
2023-03-15 20:43:57 +0000

    costs page redirects to priorities

---
+++ components/SolutionTable.js
@@ -0,0 +1,18 @@
+// import statements
+
+// defined state (the table described in JSON format)
+    // column that specifies the specialized site
+    // ...that the solution belongs to IOT color it
+// Jye and anyone else can edit this
+
+// state hooks [useState()]
+
+// logic to handle button clicks below
+    // "preventDefault" and re-render
+
+// logic to handle the background coloring
+    // based on which specialized site the solution belongs to
+// functional component
+    // button(s) to select arrangement order
+    // the table displayed according to the correct arrangement
+// <style> tag in this component ".js" file to handle any custom logic
--- pages/costs/index.js
+++ pages/costs/index.js
@@ -1,27 +1,39 @@
-import Head from "next/head";
-const hierarchy = require("./hierarchy.json");
-import SpecializedSite from "../../components/SpecializedSite";
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+// import Head from "next/head";
+// const hierarchy = require("./hierarchy.json");
+// import SpecializedSite from "../../components/SpecializedSite";
 
 const siteConfig = require("./site_config.json");
-const site = siteConfig.specialized_site_name;
+// const site = siteConfig.specialized_site_name;
 
 const Costs = () => {
-  return (
-    <>
-      <Head>
-        <title>UCS | Costs Site</title>
-        <meta
-          name="keywords"
-          content="costs, cost internalization, economic, externalities, property rights"
-        />
-      </Head>
-      <SpecializedSite
-        site={site}
-        focusAreas={hierarchy}
-        /* siteDescription={"Internalized vs. subsidized"} */
-      />
-    </>
-  );
+  const router = useRouter();
+  // Since there's nothing else in the costs page, 
+  // ...we are redirecting to the priorities page
+  useEffect(() => {
+    // Perform client-side navigation to the /about/team route
+    router.replace('/costs/economy');
+  }, []);
+
+  return null;
+  
+  // return (
+  //   <>
+  //     <Head>
+  //       <title>UCS | Costs Site</title>
+  //       <meta
+  //         name="keywords"
+  //         content="costs, cost internalization, economic, externalities, property rights"
+  //       />
+  //     </Head>
+  //     <SpecializedSite
+  //       site={site}
+  //       focusAreas={hierarchy}
+  //       /* siteDescription={"Internalized vs. subsidized"} */
+  //     />
+  //   </>
+  // );
 };
 
 export default Costs;
