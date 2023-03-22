65f53ddae6b25f849c258f42dad9112ff2f0ba9d
Richard Burd <richardburd@yahoo.com>
2023-03-22 19:43:28 +0000

    removed redundant whitespace

--- pages/accomplishments.js
+++ pages/accomplishments.js
@@ -87,7 +87,7 @@ const Accomplishments = () => {
         </div>
         <div className="pb-6">
           <b>2021-Present:</b>&nbsp;As of { dateOfLastChange }, 23 of our over-200 solutions contain at-least-mostly-full sets of estimates.&nbsp;  The combined totals are 
-          ${ totalCostInTrillions } trillion U.S. dollars per year in costs, ${ totalBenefitInTrillions } trillion U.S. dollars per year in benefits, { totalMTCO2eInMillions } &nbsp; billion tons of carbon dioxide-equivalents per year of greenhouse gas reduction, and { totalHabitatPreservedMillionsKm2 } million square kilometers of habitat preserved.
+          ${ totalCostInTrillions } trillion U.S. dollars per year in costs, ${ totalBenefitInTrillions } trillion U.S. dollars per year in benefits, { totalMTCO2eInMillions } billion tons of carbon dioxide-equivalents per year of greenhouse gas reduction, and { totalHabitatPreservedMillionsKm2 } million square kilometers of habitat preserved.
         </div>
         <div className="pb-6">
           <b>2020-Present:</b>&nbsp;Invented and developed
