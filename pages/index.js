fef662df2fbe527c897790d59f38d1a7db93154f
richardburd <rick.a.burd@gmail.com>
2023-04-09 15:05:52 -0700

    swoosh implemented on homepage

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,8 @@ import HomepageSpecializedSiteButton from "../components/HomepageSpecializedSite
 
 // This tracks how many visitors come to the webpage
 // https://vercel.com/docs/concepts/analytics/audiences/quickstart
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
+export const path = `${process.env.NEXT_PUBLIC_ARTICLE_IMAGES_URI_PATH}`;
 
 export default function Home() {
   return (
@@ -20,13 +21,13 @@ export default function Home() {
         Welcome!
       </div>
       <div className="flex justify-between">
-        <div className="hidden lg:block">
+        <div className="hidden lg:block mt-8">
           <Image
-            className="opacity-70"
-            src={`/images/ucs-logo.svg`}
+            className=""
+            src={`${path}/${"swoosh_4_optimized.svg"}`}
             alt={"UCS Logo"}
-            height={340}
-            width={620}
+            height={440}
+            width={1024}
             priority // https://nextjs.org/docs/basic-features/image-optimization#priority
           />
         </div>
@@ -40,19 +41,19 @@ export default function Home() {
             priority // https://nextjs.org/docs/basic-features/image-optimization#priority
           />
         </div>
-        <div className="hidden lg:block pt-32 pr-4 standard-font-1 standard-font-color-1 text-6xl">
-          Welcome!
-        </div>
       </div>
-      <div className="pb-6 pl-4 lg:pl-0 standard-font-1 standard-font-color-1 text-2xl sm:text-4xl lg:text-6xl">
-        Our specialized sites:
-      </div>
+
       {/* <--mobile view--> 770px <--tablet view--> 1025px <--desktop view--> */}
       {/* If the viewport is less than 770px, render mobile option */}
       {/* else if th viewport is less than 1025px, render tablet option */}
       {/* finally, if neither of those conditions are met, render desktop option */}
-      <div className={"grid grid-cols-1 pl-4 lg:pl-0 md:grid-cols-2 lg:grid-cols-3 gap-x-2"}>
-        <HomepageSpecializedSiteButton site="history" />        <HomepageSpecializedSiteButton site="energy" />
+      <div
+        className={
+          "grid grid-cols-1 pl-4 lg:pl-0 md:grid-cols-2 lg:grid-cols-3 gap-x-2 mt-1 -translate-y-14"
+        }
+      >
+        <HomepageSpecializedSiteButton site="history" />{" "}
+        <HomepageSpecializedSiteButton site="energy" />
         <HomepageSpecializedSiteButton site="matter" />
         <HomepageSpecializedSiteButton site="habitat" />
         <HomepageSpecializedSiteButton site="cities" />
@@ -60,7 +61,6 @@ export default function Home() {
         <HomepageSpecializedSiteButton site="oceans" />
         <HomepageSpecializedSiteButton site="space" />
         <HomepageSpecializedSiteButton site="costs" />
-
       </div>
       <Analytics />
     </>
