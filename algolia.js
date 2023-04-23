2e8992f002673cbfb9157c90dccfe00b22f4256e
richardburd <rick.a.burd@gmail.com>
2023-04-23 10:37:18 -0700

    algolia config files

--- algolia.js
+++
@@ -1,6 +0,0 @@
-import algoliasearch from 'algoliasearch/lite';
-
-const client = algoliasearch('ALGOLIA_APP_ID', 'ALGOLIA_SEARCH_KEY');
-const index = client.initIndex('ALGOLIA_INDEX_NAME');
-
-export { client, index };
---
+++ scripts/importToAlgolia.js
@@ -0,0 +1,52 @@
+// scripts/importToAlgolia.js
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import algoliasearch from "algoliasearch";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Read and parse all pages in the "pages" directory
+function getAllPages(dir) {
+  const pages = fs.readdirSync(dir);
+  const pageData = [];
+
+  pages.forEach((filename) => {
+    const filePath = path.join(dir, filename);
+    const fileContents = fs.readFileSync(filePath, "utf8");
+    const { data, content } = matter(fileContents);
+
+    // Ignore pages you don't want to index, like _app.js or _document.js
+    if (!filename.startsWith("_") && !filename.startsWith("api")) {
+      pageData.push({
+        objectID: filename,
+        title: data.title || filename,
+        url: `/${filename.replace(/\.mdx?$/, "")}`,
+        content,
+      });
+    }
+  });
+
+  return pageData;
+}
+
+async function importPagesToAlgolia() {
+  const client = algoliasearch(
+    process.env.ALGOLIA_APP_ID,
+    process.env.ALGOLIA_API_KEY
+  );
+
+  const index = client.initIndex("your_index_name"); // Replace with your Algolia index name
+
+  const pages = getAllPages("./pages");
+
+  try {
+    await index.saveObjects(pages);
+    console.log("Pages imported to Algolia successfully!");
+  } catch (error) {
+    console.error("Error importing pages to Algolia:", error);
+  }
+}
+
+importPagesToAlgolia();
