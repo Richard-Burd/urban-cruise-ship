feb34e45717912b42705064dda17fe9063155c2a
richardburd <rick.a.burd@gmail.com>
2023-04-30 16:51:30 -0700

    more styling & files for test data import

---
+++ data/endeavorTestData.js
@@ -0,0 +1,26 @@
+export const endeavorData = [
+    {
+      endeavor: "Anti-Malaria Campaign, China (2005-2019)",
+      link: "/history/endeavors#public-health-projects",
+      site: "history",
+      cost: 0.01239,
+      benefit: 2.37,
+      co2: null,
+      habitat: null,
+      sources: [
+        `[^2]: Sudathip, P., Kongkasuriyachai, D., Stelmach, R., Bisanzio, D., Sine, J., Sawang, S., Kitchakarn, S., Sintasath, D., Reithinger, R. ["The Investment Case for Malaria Elimination in Thailand: A Cost–Benefit Analysis"](https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6553898/). The American journal of tropical medicine and hygiene 100(6), pp. 1445-1453. June 2019.`,
+      ],
+    },
+    {
+      endeavor: "Smallpox Eradication, World (1959-1977)",
+      link: "/history/endeavors#public-health-projects",
+      site: "history",
+      cost: 1.48,
+      benefit: 234.37,
+      co2: null,
+      habitat: null,
+      sources: [
+        `[^3]: Barrett, S. ["Economic considerations for the eradication endgame"](https://dx.doi.org/10.1098%2Frstb.2012.0149). Philosophical Transactions of the Royal Society B: Biological Sciences 368(1623): 20120149. August 2013.`,
+      ],
+    },
+  ];
