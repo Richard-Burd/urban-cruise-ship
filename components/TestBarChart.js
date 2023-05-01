d960dfe1ca9821b612da29679dd03eb4dd939667
richardburd <rick.a.burd@gmail.com>
2023-05-01 11:50:39 -0700

    recharts dynamic background color

--- components/TestBarChart.js
+++ components/TestBarChart.js
@@ -1,7 +1,3 @@
-import { ResponsiveContainer } from 'recharts';
-import endeavorData from '../data/endeavorTestData';
-import solutionData from '../data/solutionTestData';
-
 import React from "react";
 import {
   Bar,
@@ -13,11 +9,16 @@ import {
 } from "recharts";
 
 const data = [
-  { name: "High Speed Rail, Hong Kong to China (2010 - 2060)", barlength: 4000, displayedValue: "Three Hundred" },
+  {
+    name: "High Speed Rail, Hong Kong to China (2010 - 2060)",
+    barlength: 4000,
+    displayedValue: "Three Hundred",
+  },
   {
     name: "Test Row B",
     barlength: 1200,
     displayedValue: "One Thousand Two Hundred",
+    backgroundColor: "#FCD34D",
   },
   {
     name: "Test Row C",
@@ -35,7 +36,7 @@ const customLabelRenderer = (props) => {
   return (
     <text
       x={x + width + 10}
-      y={y + 15}
+      y={y + 10}
       textAnchor="start"
       fill="black"
       fontFamily="Roboto"
@@ -46,54 +47,84 @@ const customLabelRenderer = (props) => {
   );
 };
 
+const CustomYAxisTick = ({ x, y, payload, backgroundColor }) => {
+  return (
+    <g transform={`translate(${x},${y})`}>
+      <rect
+        x={-300}
+        y={-10}
+        width={300}
+        height={20}
+        fill={backgroundColor || "transparent"}
+      />
+      <text
+        x={0}
+        y={0}
+        dy={5}
+        textAnchor="end"
+        fill="#000"
+        fontFamily="Roboto"
+        fontSize="15px"
+        fontWeight="bold"
+      >
+        {payload.value}
+      </text>
+    </g>
+  );
+};
+
 const TestBarChart = () => {
   return (
     <>
       <h2>This chart is a test under construction</h2>
-      <ResponsiveContainer width="100%" height={300}>
-      <BarChart
-        width={800}
-        height={300}
-        data={data}
-        layout="vertical"
-        margin={{
-          top: 20,
-          right: 120,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <XAxis type="number" hide={true} />
-        <YAxis
-          type="category"
-          dataKey="name"
-          width={400}
-          tick={{
-            fill: "black",
-            fontFamily: "Roboto",
-            fontSize: "15px",
-            fontWeight: "bold",
-            width: 200,
-            lineHeight: 1.5
+      <div style={{ width: "100%", height: 400 }}>
+        <BarChart
+          width={1000}
+          height={400}
+          data={data}
+          layout="vertical"
+          margin={{
+            top: 20,
+            right: 120,
+            left: 0,
+            bottom: 5,
           }}
-          axisLine={{ stroke: "#000", strokeWidth: 0 }}
-          tickLine={{ stroke: "#000", strokeWidth: 0 }}
-        />
-        <Tooltip />
-        <Bar dataKey="barlength" fill="#171717" minPointSize={5} barSize={20}>
-          <LabelList
-            dataKey="displayedValue"
-            position="right"
-            content={customLabelRenderer}
+        >
+          <XAxis type="number" hide={true} />
+          <YAxis
+            type="category"
+            dataKey="name"
+            width={300}
+            tickLine={false}
+            axisLine={false}
+            tick={(props) => {
+              const entry = data.find((e) => e.name === props.payload.value);
+              return (
+                <CustomYAxisTick
+                  {...props}
+                  backgroundColor={
+                    entry ? entry.backgroundColor : "transparent"
+                  }
+                />
+              );
+            }}
           />
-        </Bar>
-      </BarChart>
-      </ResponsiveContainer>
+          <Tooltip />
+          <Bar dataKey="barlength" fill="#171717" barSize={12}>
+            <LabelList
+              dataKey="displayedValue"
+              position="right"
+              content={customLabelRenderer}
+            />
+          </Bar>
+        </BarChart>
+      </div>
     </>
   );
 };
 
 export default TestBarChart;
+
 
 
 
@@ -190,3 +221,66 @@ export default TestBarChart;
 // };
 
 // export default TestBarChart;
+
+
+
+
+
+
+
+
+
+// To wrap the text inside the CustomYAxisTick component after it exceeds 200 pixels in length, you can create a custom text-wrapping function. Here's an updated CustomYAxisTick component with a text-wrapping function:
+
+// const CustomYAxisTick = ({ x, y, payload, backgroundColor }) => {
+//   const wrapText = (text, maxWidth) => {
+//     const words = text.split(" ");
+//     const lines = [];
+//     let line = [];
+
+//     words.forEach((word) => {
+//       const currentLine = line.join(" ");
+//       const newLine = currentLine + (currentLine.length > 0 ? " " : "") + word;
+//       const newLineWidth = newLine.length * 7; // 7px is an estimated width for each character in the 15px Roboto font
+
+//       if (newLineWidth <= maxWidth) {
+//         line.push(word);
+//       } else {
+//         lines.push(line.join(" "));
+//         line = [word];
+//       }
+//     });
+
+//     lines.push(line.join(" "));
+//     return lines;
+//   };
+
+//   const wrappedText = wrapText(payload.value, 200);
+
+//   return (
+//     <g transform={`translate(${x},${y})`}>
+//       <rect
+//         x={-300}
+//         y={-10}
+//         width={300}
+//         height={20}
+//         fill={backgroundColor || "transparent"}
+//       />
+//       {wrappedText.map((line, index) => (
+//         <text
+//           key={`tick-text-${index}`}
+//           x={0}
+//           y={index * 20}
+//           dy={5}
+//           textAnchor="end"
+//           fill="#000"
+//           fontFamily="Roboto"
+//           fontSize="15px"
+//           fontWeight="bold"
+//         >
+//           {line}
+//         </text>
+//       ))}
+//     </g>
+//   );
+// };
