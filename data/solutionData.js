60491afe6b3328c3179ffdec7826a096b6b439fc
Lee Nelson <116308791+Lee-Nelson-UCS@users.noreply.github.com>
2024-03-16 12:34:21 +0000

    upgrade power grid solution

--- data/solutionData.js
+++ data/solutionData.js
@@ -852,7 +852,20 @@ export const solutionData = [
     habitat: null,
     sources: null,
     subset: "energy-production-efficiency",
+  },
+
+  {
+    solution: `Upgrade Power Grid - U.S.`,
+    link: `/energy/energy_distribution/grid_overview`,
+    site: `energy`,
+    cost: 40,
+    benefit: 112,
+    co2: null,
+    habitat: null,
+    sources: null,
+    subset: "energy-production-efficiency",
   }
+
 ];
 
 /*   { //I have removed this solution because there were no costs that could be found on it. Perhaps there is some sort of solution that we can come up with, or perhaps there are some hidden costs that I have not found as of yet. Otherwise, we could write into the logic so that this presents a special string such as "No known costs" -jye 5/23/2023
