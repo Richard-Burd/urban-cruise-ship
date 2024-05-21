82214cb39472ad47c5c8398f3426e305fff33382
Michael Goff <michaelgoff@Michaels-MBP.home>
2024-05-21 14:42:45 -0700

    Offshore wind revisions

---
+++ documentation/offshore-wind.py
@@ -0,0 +1,21 @@
+'''
+Figures related to offshore wind.
+Documentation created: May 21, 2024. Last substantive update: May 21, 2024.
+'''
+
+# https://gwec.net/gwecs-global-offshore-wind-report-2023/
+# In GW
+offshore_wind_capacity = {
+    "2022":64.3
+}
+
+# Additions, in MW. Array is from 2006 to 2022.
+offshore_wind_additions = [90, 208, 459, 622, 1031, 1002, 1306, 1674, 1684, 3382, 2223, 4472, 4296, 6243, 6852, 21106, 8771]
+# The sum of these values is greater than the 2022 capacity. My guess is that the reason is that they include additions only and not retirements.
+
+
+
+
+# https://wfo-global.org/wp-content/uploads/2024/04/WFO-Report-2024Q1.pdf
+# In MW. List from 2014 to 2023, with I think the last value being a projection.
+offshore_wind_capacity_wfo = [7893, 11561, 12815, 17031, 22110, 27304, 32510, 48176, 57609, 67412]
