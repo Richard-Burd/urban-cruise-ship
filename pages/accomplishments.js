49596b7105b0e9322b684c3f0e723371cfaa7fa5
Lee Nelson <116308791+Lee-Nelson-UCS@users.noreply.github.com>
2024-08-21 21:55:48 +0000

    accomplishments updated

--- pages/accomplishments.js
+++ pages/accomplishments.js
@@ -71,10 +71,10 @@ const Accomplishments = () => {
           extensively previously by numerous other organizations.
         </div>
         <div className="pb-6" id="metrics">
-          <b>2021-Present:</b>&nbsp;As of August 5, 2024, 103 of our over-220
+          <b>2021-Present:</b>&nbsp;As of August 5, 2024, 110 of our over-220
           solutions contain at-least-mostly-full sets of estimates.&nbsp; The
-          combined totals are $6.77 trillion U.S. dollars per year in costs,
-          $13.78 trillion U.S. dollars per year in benefits, 43.52 billion tons
+          combined totals are $7.74 trillion U.S. dollars per year in costs,
+          $14.8 trillion U.S. dollars per year in benefits, 45.88 billion tons
           of carbon dioxide-equivalents per year of greenhouse gas reduction,
           and 304 billion square kilometers of habitat preserved.
         </div>
